1bdb2ac38ce35893163dcb9687526ff36d6ba82a
Aksel Hartikainen <akselpoleminuparisnimi@gmail.com>
2026-04-23 16:35:08 +0300

    add

---
+++ app.js
@@ -0,0 +1,25 @@
+const images = ['1,jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
+const targetimage = document.querySelector('.cover-image img');
+let i = 1;
+
+targetimage.addEventListener('click', () => {
+
+    targetimage.style.opacity = '0';
+
+    setTimeout(() => {
+       if(i === images.length - 1) {
+        i = 0;
+    } 
+    
+    targetimage.src = 'images/' + images[i];
+    i++;
+
+    targetimage.style.opacity = '1';
+
+}, 500);
+
+    
+
+    
+    
+});
