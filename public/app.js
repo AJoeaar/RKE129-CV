5774bb01c099ea35107a9509f2cacab2880d04d7
Aksel Hartikainen <akselpoleminuparisnimi@gmail.com>
2026-05-06 21:52:54 +0300

    add week

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const images = ['1,jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
+const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
 const targetimage = document.querySelector('.cover-image img');
 let i = 1;
 
